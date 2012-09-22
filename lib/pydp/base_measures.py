b814f4c3ef237da85c66e08133d2ba49e273e305
andrew <andrew@localhost>
2012-09-22 10:36:38 -0700

    Simplified instantion of base measures

--- lib/pydp/base_measures.py
+++ lib/pydp/base_measures.py
@@ -5,16 +5,13 @@ Created on 2012-09-21
 '''
 from __future__ import division
 
+from pydp.data import BetaData, BetaParameter, GammaData, GammaParameter
 from pydp.rvs import beta_rvs, gamma_rvs
-from pydp.data import BetaData, GammaData
 
 class BaseMeasure(object):
     '''
     Base class for base measures.
     '''
-    def __init__(self, params):
-        self.params = params
-    
     def random(self):
         '''
         Return a random sample from the base measure.
@@ -22,12 +19,18 @@ class BaseMeasure(object):
         pass
     
 class BetaBaseMeasure(BaseMeasure):
+    def __init__(self, a, b):
+        self.params = BetaParameter(a, b)
+    
     def random(self):
         x = beta_rvs(self.params.a, self.params.a)
         
         return BetaData(x)
     
 class GammaBaseMeasure(BaseMeasure):
+    def __init__(self, a, b):
+        self.params = GammaParameter(a, b)
+    
     def random(self):
         x = gamma_rvs(self.params.a, self.params.b)
         
