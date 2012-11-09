a8f3a95297a67dbdd9ec3f7ac8fa2e402ad77cbc
andrew <andrew@localhost>
2012-11-09 14:05:43 -0800

    - Added setup files.

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+      name='PyDP',
+      version='0.1.0',
+      description='A Python library for implementing Dirichlet process mixture models.',
+      author='Andrew Roth',
+      author_email='andrewjlroth@gmail.com',
+      url='https://bitbucket.org/aroth85/pydp',
+      package_dir = {'': 'lib'},    
+      packages=[ 
+                'pydp',
+                'pydp.samplers'
+                ]
+     )
