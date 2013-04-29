108c40fbfa888470390f4bc3e82be8eced5f8c8f
andrew <andrew@localhost>
2013-04-29 10:42:31 -0700

    Updated DP sampler to print global params nicely.

--- lib/pydp/samplers/dp.py
+++ lib/pydp/samplers/dp.py
@@ -14,25 +14,37 @@ Created on 2013-03-21
 
 @author: Andrew Roth
 '''
+from __future__ import division
+
+from collections import OrderedDict, namedtuple
+
 from pydp.partition import Partition
 from pydp.samplers.concentration import GammaPriorConcentrationSampler
 
 class DirichletProcessSampler(object):
-    def __init__(self, atom_sampler, partition_sampler, alpha=None, alpha_shape=1, alpha_rate=1):
+    def __init__(self, atom_sampler, partition_sampler, alpha=1.0, alpha_priors=None, global_params_sampler=None):
         self.atom_sampler = atom_sampler
         
-        self.partition_sampler = partition_sampler           
+        self.partition_sampler = partition_sampler
         
-        if alpha is None:
-            self.alpha = 1
-            
+        self.alpha = alpha          
+        
+        if alpha_priors is None:
+            self.update_alpha = False
+
+        else:
             self.update_alpha = True
             
-            self.concentration_sampler = GammaPriorConcentrationSampler(alpha_shape, alpha_rate)
+            self.concentration_sampler = GammaPriorConcentrationSampler(alpha_priors['shape'], 
+                                                                        alpha_priors['rate']) 
+        
+        if global_params_sampler is None:
+            self.update_global_params = False
+        
         else:
-            self.alpha = alpha
+            self.update_global_params = True
             
-            self.update_alpha = False 
+            self.global_params_sampler = global_params_sampler
         
         self.num_iters = 0
     
@@ -41,23 +53,54 @@ class DirichletProcessSampler(object):
         return {
                 'alpha' : self.alpha,
                 'labels' : self.partition.labels,
-                'params' : [param for param in self.partition.item_values]
+                'params' : [param for param in self.partition.item_values],
+                'global_params' : self.atom_sampler.cluster_density
                 }
     
-    def initialise_partition(self, data):
+    def initialise_partition(self, data, init_method):
+        '''
+        Args:
+            data : (list) Data points.
+            
+        Kwargs:
+            method : (str) Initialisation method to use. 
+                           - 'separate' will allocate each data point to a separate partition.
+                           - 'together' will allocate all data points to the same partition.
+        '''
+        
         self.partition = Partition()
         
-        for item, _ in enumerate(data):
+        if init_method == 'separate':
+            for item, _ in enumerate(data):
+                self.partition.add_cell(self.partition_sampler.base_measure.random())
+                
+                self.partition.add_item(item, item)
+        
+        elif init_method == 'together':
             self.partition.add_cell(self.partition_sampler.base_measure.random())
             
-            self.partition.add_item(item, item)        
+            for item, _ in enumerate(data):                
+                self.partition.add_item(item, 0)
+                 
     
-    def sample(self, data, trace, num_iters, print_freq=100):
-        self.initialise_partition(data)
+    def sample(self, data, trace, num_iters, init_method='separate', print_freq=100):
+        self.initialise_partition(data, init_method)
         
         for i in range(num_iters):
             if i % print_freq == 0:
-                print self.num_iters, self.partition.number_of_cells, self.alpha
+                print self.num_iters, self.partition.number_of_cells, self.alpha 
+                
+                if self.update_global_params:
+                    params = self.atom_sampler.cluster_density.params
+                    
+                    if isinstance(params, OrderedDict):
+                        print ','.join([str(x[0]) for x in self.atom_sampler.cluster_density.params.values()])
+                    
+                    elif isinstance(params, tuple):
+                        print params[0]
+                    
+                    else:
+                        raise Exception('Object type {0} is not a valid cluster parameter'.format(type(params)))
             
             self.interactive_sample(data)
             
@@ -74,11 +117,6 @@ class DirichletProcessSampler(object):
         self.partition_sampler.sample(data, self.partition, self.alpha)
         
         self.atom_sampler.sample(data, self.partition)
-    
-    def _init_partition(self, base_measure):
-        self.partition = Partition()
         
-        for item, _ in enumerate(self.data):
-            self.partition.add_cell(base_measure.random())
-            
-            self.partition.add_item(item, item)
+        if self.update_global_params:
+            self.global_params_sampler.sample(data, self.partition)
