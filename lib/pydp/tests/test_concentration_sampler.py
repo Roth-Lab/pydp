9ef8a4cb73a61f15607e70136b768bb37978356f
andrew <andrew@localhost>
2013-05-21 16:20:25 -0700

    Added rough test for concentration sampler.

---
+++ lib/pydp/tests/test_concentration_sampler.py
@@ -0,0 +1,16 @@
+from math import log
+
+from pydp.samplers.concentration import GammaPriorConcentrationSampler
+
+sampler = GammaPriorConcentrationSampler(0.01, 0.01)
+
+x = 0
+
+for i in range(1000000):
+    x = sampler.sample(x, 1, 100)
+    
+    x /= 2
+    
+    print x
+
+    log(x)
