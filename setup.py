e61b88273563c73f8ec91a90a7ad45b6726f51d7
Andrew Roth <andrewjlroth@gmail.com>
2018-01-06 17:01:47 +0000

    minor: bumping version

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import find_packages, setup
 
 setup(
-      name='PyDP',
-      version='0.2.3',
-      description='A Python library for implementing Dirichlet process mixture models.',
-      author='Andrew Roth',
-      author_email='andrewjlroth@gmail.com',
-      url='https://bitbucket.org/aroth85/pydp',
-      packages=find_packages(),
-      license = 'GPL v3'
-     )
+    name='PyDP',
+    version='0.2.4',
+    description='A Python library for implementing Dirichlet process mixture models.',
+    author='Andrew Roth',
+    author_email='andrewjlroth@gmail.com',
+    url='https://bitbucket.org/aroth85/pydp',
+    packages=find_packages(),
+    license='GPL v3'
+)
